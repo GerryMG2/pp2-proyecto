bba1a3a7b654e160e2f5f4b16a75f127d1e77125
GerryMG2 <00032515@uca.edu.sv>
2021-10-23 17:13:22 -0600

    error of an error

--- utils/ScriptManager.js
+++ utils/ScriptManager.js
@@ -20,45 +20,68 @@ class ScriptManager {
 
         console.log(obj);
 
-
-        let forked = fork("./Scripts/" + obj.script);
-        let ob = {
-            data: obj.data,
-            conn: obj.connection
-        }
-        forked.on("message", (msg) => {
-            if (msg.send) {
-                forked.send(ob);
-            } else {
-                console.log("msg: ")
-                console.log(msg);
-                if (msg.status) {
-
-                } else {
-
-                    let err = errors.create({
-                        script: obj.script,
-                        log: msg.log,
-                        data: obj.data,
-                        idTransaction: obj._id.id
-                    });
-
-                    err.save().then((L) => {
-                        console.log(URL+'/adderrors');
-                        request(URL + '/adderrors', function (error, response, body) {
-                            console.log(response);
-                            console.log(error);
-                            if (!error && response.statusCode == 200) {
-                                console.log(body) // Print the google web page.
-                            }
-                        })
-                        console.log("save Error");
-                    });
-                }
-                forked.kill(2);
+        try {
+            let forked = fork("./Scripts/" + obj.script);
+            let ob = {
+                data: obj.data,
+                conn: obj.connection
             }
+            forked.on("message", (msg) => {
+                if (msg.send) {
+                    forked.send(ob);
+                } else {
+                    console.log("msg: ")
+                    console.log(msg);
+                    if (msg.status) {
 
-        });
+                    } else {
+
+                        let err = errors.create({
+                            script: obj.script,
+                            log: msg.log,
+                            data: obj.data,
+                            idTransaction: obj._id.id
+                        });
+
+                        err.save().then((L) => {
+                            console.log(URL + '/adderrors');
+                            request(URL + '/adderrors', function (error, response, body) {
+                                console.log(response);
+                                console.log(error);
+                                if (!error && response.statusCode == 200) {
+                                    console.log(body) // Print the google web page.
+                                }
+                            })
+                            console.log("save Error");
+                        });
+                    }
+                    forked.kill(2);
+                }
+
+            });
+        } catch (error) {
+            let err = errors.create({
+                script: obj.script,
+                log: error,
+                data: obj.data,
+                idTransaction: obj._id.id
+            });
+
+            err.save().then((L) => {
+                console.log(URL + '/adderrors');
+                request(URL + '/adderrors', function (error, response, body) {
+                    console.log(response);
+                    console.log(error);
+                    if (!error && response.statusCode == 200) {
+                        console.log(body) // Print the google web page.
+                    }
+                })
+                console.log("save Error");
+            });
+        }
+
+
+
 
 
 
